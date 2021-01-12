48098cdb38948df749c34ccfb046bf7800bdaed8
gregoryjtom <gregoryjtom@gmail.com>
2021-01-12 00:41:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+**REPOSITORY GUIDELINES**
+
+*To run weekly assignments:*
+
+1. Go to shell, and navigate to WeekX folder, where X = week of project: `cd WeekX`
+2. Compile java file: `javac WordCounter.java`
+3. Run java file: `java WordCounter`
+
+*General:*
+
+1. Files for respective weeks can be found in WeekX directory, where X = week of project. 
+2. Text files are in parent directory and are referenced in the Java code as such.
+3. Main.java file in parent directory serves no purpose.
+
+Sources:
+- Reading files: https://www.w3schools.com/java/java_files_read.asp
+- Sorting hashmap: https://www.educative.io/edpresso/how-to-sort-a-java-hashmap-by-value
---
+++ Week1/WordCounter.java
@@ -0,0 +1,107 @@
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.*;
+import java.util.regex.*;
+
+class WordCounter {
+  public static void main(String[] args) {
+    ArrayList<String> stop_words = loadStopWords();
+    HashMap<String, Integer> frequencies = countWordsInFile("../pride-and-prejudice.txt",stop_words);
+
+    ArrayList<Map.Entry<String, Integer>> freq_list = sortFrequencies(frequencies);
+
+    // print 25 most frequent entries
+    System.out.println("----TOP 25 WORDS----");
+    int i = 1;
+    for (Map.Entry<String, Integer> entry : freq_list) {
+      if (i++ > 25) {
+        break;
+      }
+      System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+  }
+
+  private static ArrayList<String> loadStopWords() {
+    ArrayList<String> stop_words = new ArrayList<String>();
+    try {
+      // load stop words file
+      File stop_words_file = new File("../stop_words.txt");
+      Scanner myReader = new Scanner(stop_words_file);
+      String list = "";
+      // read the one line into a string and split
+      while (myReader.hasNext()) {
+        list = myReader.next();
+      }
+      String[] split_stop_words = list.split(",");
+      // add each word to ArrayList
+      for (int i = 0; i < split_stop_words.length; i++) {
+        stop_words.add(split_stop_words[i]);
+      }
+      myReader.close();
+
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    return stop_words;
+  }
+
+  private static HashMap<String, Integer> countWordsInFile(String filepath, ArrayList<String> stop_words) {
+    // make HashMap for frequencies
+    HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
+    try {
+      // analyze text file line by line
+      File text_file = new File(filepath);
+      Scanner myReader = new Scanner(text_file);
+      while (myReader.hasNextLine()) {
+        String line = myReader.nextLine().toLowerCase();
+        // search for words
+        Pattern pattern = Pattern.compile("[a-z]{2,}");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+          String w = matcher.group();
+          if (!stop_words.contains(w)) {
+            if (frequencies.containsKey(w)) {
+              frequencies.put(w, frequencies.get(w) + 1);
+            } else {
+              frequencies.put(w, 1);
+            }
+          }
+        }
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    return frequencies;
+  }
+
+  private static ArrayList<Map.Entry<String,Integer>> sortFrequencies(HashMap<String,Integer> frequencies){
+    // create ArrayList to store sorted list
+    ArrayList<Map.Entry<String,Integer>> freq_list = new ArrayList<>();
+
+    // put all HashMap entries into freq_list
+    for(Map.Entry<String, Integer> e: frequencies.entrySet()) 
+    {
+        freq_list.add(e);
+    }
+
+    // create comparator to use to sort list
+    Comparator<Map.Entry<String, Integer>> myComparator = new Comparator<Map.Entry<String, Integer>>() {
+        @Override
+        public int compare(
+              Map.Entry<String, Integer> e1,
+              Map.Entry<String, Integer> e2) {
+
+            Integer int1 = e1.getValue();
+            Integer int2 = e2.getValue();
+            return int2.compareTo(int1);
+        }
+    };
+
+    // sort and return
+    Collections.sort(freq_list,myComparator);
+    return freq_list;
+  }
+}
